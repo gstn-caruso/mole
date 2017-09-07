253a6d9d1dd6f13ec7feac61af051fa4233e5168
Gastón Caruso <gstn.caruso@gmail.com>
2017-09-06 23:55:59 -0300

    Initial commit

---
+++ desktop/src/com/gaston/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gaston.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gaston.Mole;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Mole(), config);
+	}
+}
