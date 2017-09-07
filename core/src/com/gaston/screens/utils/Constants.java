adb089d3d7b213aaf0559783e0d53b99caf037ca
Gaston Caruso <gstn.caruso@gmail.com>
2017-09-07 00:29:01 -0300

    Agrego plataforma básica

---
+++ core/src/com/gaston/screens/utils/Constants.java
@@ -0,0 +1,14 @@
+package com.gaston.screens.utils;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Constants {
+    public static final int APP_WIDTH = 800;
+    public static final int APP_HEIGHT = 480;
+    static final Vector2 WORLD_GRAVITY = new Vector2(0, -10);
+    static final float GROUND_X = 0;
+    static final float GROUND_Y = 0;
+    static final float GROUND_WIDTH = 25f;
+    static final float GROUND_HEIGHT = 2f;
+    static final float GROUND_DENSITY = 0f;
+}
--- desktop/src/com/gaston/desktop/DesktopLauncher.java
+++ desktop/src/com/gaston/desktop/DesktopLauncher.java
@@ -3,10 +3,13 @@ package com.gaston.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.gaston.Mole;
+import com.gaston.screens.utils.Constants;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = Constants.APP_WIDTH;
+		config.height = Constants.APP_HEIGHT;
 		new LwjglApplication(new Mole(), config);
 	}
 }
