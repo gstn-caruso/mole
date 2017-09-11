0006581968e050650caac0c90d872c5dd9577167
Gastón Caruso <gstn.caruso@gmail.com>
2017-09-10 22:59:24 -0300

    Jumping

--- core/src/com/gaston/character/Player.scala
+++ core/src/com/gaston/character/Player.scala
@@ -9,17 +9,25 @@ import com.gaston.scenario.Ground
 
 class Player extends RectangularPositioned with SpeedBehaviour {
   val basePlayerSpeedMagnitude = 800
-
+  val gravity = new Vector2(0, -5)
   position = new Vector2(10, 300)
+  var velocity = new Vector2()
+  var jump = false
+  var canJump = true
+  var standOnGround = true
+
   override val width = 15f
   override val height = 80f
   var acceleration = new Vector2(0, -250f)
 
   def update(delta: Float, ground: Ground) = {
-    checkCollisionAgainst(ground)
+    velocity.add(gravity)
     processMovement(delta)
     speed.add(acceleration.cpy().scl(delta))
     position.add(speed.cpy().scl(delta))
+    checkCollisionAgainst(ground)
+
+    //    position.add(velocity)
   }
 
   def checkCollisionAgainst(ground: Ground) {
@@ -35,10 +43,28 @@ class Player extends RectangularPositioned with SpeedBehaviour {
     if (Gdx.input.isKeyPressed(Keys.LEFT)) {
       speed.add(-200, 0)
     }
+    if (Gdx.input.isKeyPressed(Keys.UP)) {
+      if (jump && canJump) { // Add an upward velocity
+        velocity.add(0, 20)
+        // Disallow jumping, so you can't jump in mid air.
+        canJump = false
+      }
+    }
+    if (standOnGround) { // You can't fall down when you stand on a ground
+      velocity.y = 0
+      // When you stand on a ground you can jump again
+      canJump = true
+    }
+    if (velocity.y < -20) velocity.y = -20
+
   }
 
   def stopFalling {
+    standOnGround = true
     speed = new Vector2(0, 0)
     acceleration = new Vector2(0, 0)
+    canJump = true
+    velocity.y = 0
+
   }
 }
