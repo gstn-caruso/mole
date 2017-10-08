a3dac02390cf147221d345fce0fa51f20919d1d5
Gastón Caruso <gstn.caruso@gmail.com>
2017-10-08 20:01:53 -0300

    Mejorada la mecánica del salto, implemento contraparte

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,44 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	public float speed = 5f;
-	public float jumpSpeed = 2;
-	private Rigidbody2D rb;
-	public bool gorunded;
+	public float velocidadX = 0.2f;
+	public float fuerzaDeSalto = 800f;
+	public Transform pie;
+	public float radioPie;
+	public LayerMask suelo;
+	public bool enSuelo;
+	public bool enLaContraparte;
 
-	void Start()
-	{
-		rb = GetComponent<Rigidbody2D>();
-	}
+	void Update(){
+		enLaContraparte = transform.position.y <= -20;
 
-	void Update ()
-	{
-			if (Input.GetKey(KeyCode.LeftArrow))
-			{
-					transform.position += Vector3.left * speed * Time.deltaTime;
+		float inputX = Input.GetAxis("Horizontal");
+		float movimientoX = transform.position.x + (inputX * velocidadX);
+		transform.position = new Vector3(movimientoX, transform.position.y, 0);
+		if (inputX > 0) { transform.localScale = new Vector3(10, 10, 1); }
+		if (inputX < 0) { transform.localScale = new Vector3(-10, 10, 1); }
+
+		enSuelo = Physics2D.OverlapCircle(pie.position, radioPie, suelo);
+		if (enSuelo && Input.GetKeyDown(KeyCode.Space)) {
+			GetComponent<Rigidbody2D>().AddForce(new Vector2 (0, fuerzaDeSalto));
+		}
+
+		if(Input.GetKeyDown(KeyCode.LeftShift)) {
+			float nuevoY;
+			if(!enLaContraparte) {
+				nuevoY = transform.position.y - 39f;
+			} else {
+				nuevoY = transform.position.y + 39f;
 			}
-			if (Input.GetKey(KeyCode.RightArrow))
-			{
-					transform.position += Vector3.right * speed * Time.deltaTime;
-			}
-			if (Input.GetKeyDown(KeyCode.Space))
-			{
-				rb.AddForce(Vector2.up * jumpSpeed);
-			}
-	}
-
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		// grounded = true;
-	}
-
-	void OnTriggerStay2D(Collider2D other)
-	{
-		// grounded = true;
-	}
-
-	void OnTriggerExit2D(Collider2D other)
-	{
-		// grounded = false;
+			transform.position = new Vector3(transform.position.x, nuevoY, 0);
+		}
 	}
 }
