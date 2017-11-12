6198f52a3a10e864fbd96d74a49e6808807eeca1
Gastón Caruso <gstn.caruso@gmail.com>
2017-11-12 03:11:38 -0300

    Cambio sprites del piso

--- Assets/Scripts/CameraFollowPlayer.cs
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour {
 	public Transform Target;
-	public float SmoothSpeed = 0.1f;
+	public float SmoothSpeed = 0.05f;
 	public Vector3 Offset = new Vector3(0,2,-2f);
 
 	private void FixedUpdate() {
